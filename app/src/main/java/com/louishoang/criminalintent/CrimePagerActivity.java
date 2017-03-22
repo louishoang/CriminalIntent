e23d54bf0f69a7944a70dba4c70287a5cdbd5693
Louis Hoang <bawsing@gmail.com>
2017-03-22 14:33:58 -0700

    Add pager

--- app/src/main/java/com/louishoang/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/louishoang/criminalintent/CrimePagerActivity.java
@@ -50,5 +50,25 @@ public class CrimePagerActivity extends FragmentActivity {
         break;
       }
     }
+
+    mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+      @Override
+      public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+      }
+
+      @Override
+      public void onPageSelected(int position) {
+        Crime crime = mCrimes.get(position);
+        if(crime.getTitle() != null){
+          setTitle(crime.getTitle());
+        }
+      }
+
+      @Override
+      public void onPageScrollStateChanged(int state) {
+
+      }
+    });
   }
 }
